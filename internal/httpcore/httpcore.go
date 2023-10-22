485edcadc8e6acc53a8849b0010ee7fda76c50cb
AKP <abi@tdpain.net>
2023-10-22 13:09:59 +0100

    Move frontend serving statement to end of route declarations
    This reverts commit eef2d380bbdfeabf584fa35be0a517de0040aefa.

--- internal/httpcore/httpcore.go
+++ internal/httpcore/httpcore.go
@@ -4,17 +4,16 @@ import (
 	cryptoRand "crypto/rand"
 	"encoding/hex"
 	"encoding/json"
+	"github.com/CSSUoB/society-voting/internal/config"
+	"github.com/CSSUoB/society-voting/web"
+	"github.com/bwmarrin/go-alone"
+	validate "github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
 	"log/slog"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
-
-	"github.com/CSSUoB/society-voting/internal/config"
-	"github.com/CSSUoB/society-voting/web"
-	goalone "github.com/bwmarrin/go-alone"
-	validate "github.com/go-playground/validator/v10"
-	"github.com/gofiber/fiber/v2"
 )
 
 type endpoints struct{}
@@ -23,17 +22,6 @@ func ListenAndServe(addr string) error {
 	app := fiber.New()
 
 	e := endpoints{}
-
-	app.Use("/", func(ctx *fiber.Ctx) error {
-		_, isAuthed := getSessionAuth(ctx, authAdminUser|authRegularUser)
-
-		if isAuthed || strings.HasPrefix(ctx.OriginalURL(), "/auth/") {
-
-			return ctx.Next()
-		}
-
-		return ctx.Redirect("/auth/login")
-	}, web.GetHandler())
 
 	app.Get("/auth/login", e.authLogin)
 	app.Post("/auth/login", e.authLogin)
@@ -55,6 +43,16 @@ func ListenAndServe(addr string) error {
 	app.Post("/api/admin/election/stop", e.apiAdminStopElection)
 
 	app.Delete("/api/admin/user/delete", e.apiAdminDeleteUser)
+
+	app.Use("/", func(ctx *fiber.Ctx) error {
+		_, isAuthed := getSessionAuth(ctx, authAdminUser|authRegularUser)
+
+		if isAuthed {
+			return ctx.Next()
+		}
+
+		return ctx.Redirect("/auth/login")
+	}, web.GetHandler())
 
 	slog.Info("HTTP server alive", "address", addr, "voteCode", voteCode)
 	return app.Listen(addr)
