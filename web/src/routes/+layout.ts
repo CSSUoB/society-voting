61dd0ad0f85c7d5cf557282319e64e543e799250
Vishwas Adiga <vishwasadiga@gmail.com>
2023-10-23 00:47:02 +0100

    feat: Allow changing name

--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -1,1 +1,14 @@
+import { API } from "$lib/endpoints";
+import { redirect } from "@sveltejs/kit";
+
 export const ssr = false;
+
+export async function load({ fetch, params }) {
+    const response = await fetch(API.ME);
+    if (!response.ok) {
+        throw redirect(302, "/auth/login");
+    }
+    return {
+        user: await response.json()
+    };
+}
