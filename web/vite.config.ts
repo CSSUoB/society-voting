8eebf6142cbcf1206ae95d4f3ebff2a3ce9b1205
Vishwas Adiga <vishwasadiga@gmail.com>
2023-10-22 10:56:21 +0100

    chore: Set up vite->fiber proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,5 +2,17 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:8080',
+				changeOrigin: true
+			},
+			'/auth': {
+				target: 'http://127.0.0.1:8080',
+				changeOrigin: true
+			}
+		},
+	},
 });
