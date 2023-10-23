b6af7cbfc7939ec7a75e2e94cd3dd05d44f9001f
Vishwas Adiga <vishwasadiga@gmail.com>
2023-10-23 14:51:46 +0100

    fix: Use absolute api endpoint references

--- web/src/lib/endpoints.ts
+++ web/src/lib/endpoints.ts
@@ -1,13 +1,13 @@
 export const API = {
-    AUTH_LOGIN: `auth/login`,
-    AUTH_LOGOUT: `auth/logout`,
+    AUTH_LOGIN: `/auth/login`,
+    AUTH_LOGOUT: `/auth/logout`,
 
     ME: `/api/me`,
     ME_NAME: `/api/me/name`,
 
-    ELECTION: `api/election`,
+    ELECTION: `/api/election`,
 
-    ADMIN_ELECTION: `api/admin/election`,
-    ADMIN_USER: `api/admin/user`,
+    ADMIN_ELECTION: `/api/admin/election`,
+    ADMIN_USER: `/api/admin/user`,
     ADMIN_USER_DELETE: `/api/admin/user/delete`,
 } as const;
