4626c5c78e5fd8194cea3c0ad9c8e3652f6f452b
Vishwas Adiga <vishwasadiga@gmail.com>
2023-10-23 01:06:51 +0100

    feat: Allow logging out

--- web/src/lib/endpoints.ts
+++ web/src/lib/endpoints.ts
@@ -1,4 +1,6 @@
 export const API = {
+    AUTH_LOGIN: `auth/login`,
+    AUTH_LOGOUT: `auth/logout`,
     ME: `/api/me`,
     ME_NAME: `/api/me/name`
 } as const;
--- web/src/routes/+layout.ts
+++ web/src/routes/+layout.ts
@@ -6,7 +6,7 @@ export const ssr = false;
 export async function load({ fetch, params }) {
     const response = await fetch(API.ME);
     if (!response.ok) {
-        throw redirect(302, "/auth/login");
+        throw redirect(302, API.AUTH_LOGIN);
     }
     return {
         user: await response.json()
