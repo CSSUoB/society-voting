46bca7657fd5dd6b631cc53755645f92ee310e9a
akp <abi@tdpain.net>
2023-10-25 15:41:12 +0100

    Add presenter mode button to UI (#7)
    * Add presenter mode button
    
    * Fix formatting

---
+++ web/src/routes/presenterMode.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Button from "$lib/button.svelte";
+	import Panel from "$lib/panel.svelte";
+
+    const openInNewTab = () => {
+        window.open("/presenter", "_blank").focus()
+    }
+</script>
+
+<Panel title="Presenter Mode" headerIcon="open_in_new">
+    <p>Display the vote code and a QR code to attendees</p>
+    <br />
+    <Button kind="emphasis" text="Open" on:click={openInNewTab} />
+</Panel>
