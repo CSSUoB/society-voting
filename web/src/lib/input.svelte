d235fcaab7966c2f7bb47a918dadb41ba48fa721
Vishwas Adiga <vishwasadiga@gmail.com>
2023-10-25 12:04:35 +0100

    feat: Add styles to input element

--- web/src/lib/input.svelte
+++ web/src/lib/input.svelte
@@ -7,3 +7,13 @@
 </script>
 
 <input {name} {type} {value} />
+
+<style>
+	input {
+		height: 30px;
+		padding: 2px 12px;
+		border: 2px solid;
+		border-radius: 4px;
+		margin: 0 0 12px 0;
+	}
+</style>
