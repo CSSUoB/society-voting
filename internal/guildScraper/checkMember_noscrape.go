5e454e83e3686650043ccb5bfd2285ddbb96f7da
AKP <abi@tdpain.net>
2024-01-09 21:59:19 +0000

    Simulate admin users in noscrape mode

--- internal/guildScraper/checkMember_noscrape.go
+++ internal/guildScraper/checkMember_noscrape.go
@@ -8,9 +8,14 @@ func init() {
 	slog.Warn("Built with noscrape tag (fictional user information will be used)")
 }
 
+var hasAdmined = false
+
 func GetMember(studentID string) (*GuildMember, error) {
+	isAdmin := !hasAdmined
+	hasAdmined = true
 	return &GuildMember{
-		ID:   studentID,
-		Name: "Martin Martinson",
+		ID:                studentID,
+		Name:              "Martin Martinson",
+		IsCommitteeMember: isAdmin,
 	}, nil
 }
