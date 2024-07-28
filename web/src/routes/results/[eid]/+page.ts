6645063b5c82979718bb2c1a7867586c07a05782
Leonardo Bishop <me@leonardobishop.com>
2024-07-28 18:46:43 +0100

    Fix wrong API method

--- web/src/routes/results/[eid]/+page.ts
+++ web/src/routes/results/[eid]/+page.ts
@@ -7,10 +7,7 @@ export async function load({ fetch, params }) {
 		throw redirect(302, "/");
 	}
 
-	const response = await fetch(API.ELECTION_RESULTS, {
-		method: "POST",
-		body: JSON.stringify({ id }),
-	});
+	const response = await fetch(`${API.ELECTION_RESULTS}?election_id=${id}`);
 	if (!response.ok) {
 		throw error(response.status, { message: await response.text() });
 	}
