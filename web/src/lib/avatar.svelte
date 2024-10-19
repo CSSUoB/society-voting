46c7276a8857306e01bc7b6a6433048fd45d8b8a
Leonardo Bishop <me@leonardobishop.com>
2024-10-19 15:24:26 +0100

    Fix candidate avatars
    The previous solution of embedding boring-avatars no longer works

--- web/src/lib/avatar.svelte
+++ web/src/lib/avatar.svelte
@@ -1,42 +1,31 @@
 <script lang="ts">
+	import { AvatarBeam } from "svelte-boring-avatars";
+
 	export let name: string = "";
 	export let size: number = 48;
 
 	const colours = [
-		"ff9c00,9d68dc", // orange
-		"58cad6,4a9efe", // blue
-		"03e421,3b37b6", // green
-		"fdff8a,3b8b94", // yellow
-		"1234ff,cc22ff", // magenta
-		"cc5500,112233", // brown
+		["#ff9c00", "#9d68dc"], // orange
+		["#58cad6", "#4a9efe"], // blue
+		["#03e421", "#3b37b6"], // green
+		["#fdff8a", "#3b8b94"], // yellow
+		["#1234ff", "#cc22ff"], // magenta
+		["#cc5500", "#112233"], // brown
 	];
 
-	$: colour = colours[name.charCodeAt(0) % colours.length ?? 0];
-
-	const hash = async (data: string) => {
-		const encoder = new TextEncoder();
-		const encoded = encoder.encode(data);
-		const hashBuffer = await crypto.subtle.digest("SHA-256", encoded);
-		const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
-		const hashHex = hashArray.map((b) => b.toString(16).padStart(2, "0")).join(""); // convert bytes to hex string
-		return hashHex;
-	};
-	$: hashedName = hash(name);
+	$: colour = colours[name.charCodeAt(0) % colours.length || 0];
 </script>
 
-{#await hashedName}
-	<img src="" alt={`Minimalistic avatar representing ${name}`} height={size} width={size} />
-{:then n}
-	<img
-		src={`https://source.boringavatars.com/beam/${size}/${n}?colors=${colour}`}
-		alt={`Minimalistic avatar representing ${name}`}
-		height={size}
-		width={size}
-	/>
-{/await}
+{#key name}
+	<div class="avatar" style="--size: {size}px">
+		<AvatarBeam {size} {name} colors={colour} />
+	</div>
+{/key}
 
 <style>
-	img {
+	.avatar {
+		height: var(--size);
+		width: var(--size);
 		border: 2px solid #1c2e58;
 		border-radius: 999em;
 	}
