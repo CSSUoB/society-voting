e5955b3b1fc0068351f53c27f3852aeaaa3cccbc
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2026-02-05 17:46:49 +0000

    Refactor cache updating logic

--- internal/guildScraper/checkMember.go
+++ internal/guildScraper/checkMember.go
@@ -12,42 +12,36 @@ import (
 var (
 	cachedMembershipList              []*GuildMember
 	cachedMembershipListLock          = new(sync.RWMutex)
-	cachedMembershipListLastRefreshed time.Time
 )
 
 func GetMember(studentID string) (*GuildMember, error) {
 	cachedMembershipListLock.RLock()
 
-	if time.Now().Sub(cachedMembershipListLastRefreshed) > time.Minute*5 {
-		cachedMembershipListLock.RUnlock()
-		cachedMembershipListLock.Lock()
-
-		members, err := GetMembersList()
-
-		if err == nil {
-			cachedMembershipList = members
-			cachedMembershipListLastRefreshed = time.Now()
-		} else if cachedMembershipListLastRefreshed.IsZero() {
-			cachedMembershipListLock.Unlock()
-			return nil, fmt.Errorf("initially load membership list: %w", err)
-		} else {
-			slog.Warn("failed to refresh cached membership list", "error", err)
-		}
-
-		cachedMembershipListLock.Unlock()
-		cachedMembershipListLock.RLock()
-	}
-
-	var target *GuildMember
-
 	for _, x := range cachedMembershipList {
 		if x.ID == studentID {
-			target = x
-			break
+			cachedMembershipListLock.RUnlock()
+			return x, nil
 		}
 	}
 
 	cachedMembershipListLock.RUnlock()
 
-	return target, nil
+	cachedMembershipListLock.Lock()
+	defer cachedMembershipListLock.Unlock()
+
+	members, err := GetMembersList()
+	if err == nil {
+		cachedMembershipList = members
+	} else {
+		slog.Warn("failed to refresh cached membership list", "error", err)
+		return nil, nil
+	}
+
+	for _, x := range cachedMembershipList {
+		if x.ID == studentID {
+			return x, nil
+		}
+	}
+
+	return nil, nil
 }
